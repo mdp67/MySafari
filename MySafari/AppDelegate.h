1117f3f32707649d22b37d90ac3e94404512acd6
Mark Porcella <markporcella67@gmail.com>
2015-05-13 16:24:59 -0700

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Mark Porcella on 5/13/15.
+//  Copyright (c) 2015 Mark Porcella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Mark Porcella on 5/13/15.
+//  Copyright (c) 2015 Mark Porcella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
